813c2f167111ff2a9e9f2d88924eab969e1904a3
MantuRed <manuel31gerolin@gmail.com>
2022-06-09 18:17:05 +0200

    changed color

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
         <style>
             html, body {
                 background-color: #fff;
-                color: #636b6f;
+                color: #2580ad;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
                 height: 100vh;
